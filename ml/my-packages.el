61105a36b9956d282afc8bdc7b674cf7cb0a94e1
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-18 08:45:35 -0300

    ml/my-packages.el

--- ml/my-packages.el
+++ ml/my-packages.el
@@ -188,6 +188,11 @@
   (advice-add 'org-edit-special :after #'my-recenter-window)
   (advice-add 'org-edit-src-exit :before #'my-indent-buffer)
   (advice-add 'org-edit-src-exit :after #'my-recenter-window)
+
+  (general-unbind
+    :keymaps 'treemacs-mode-map
+    :with 'avy-goto-char-2-above
+    [remap evil-find-char-backward])
 ;; evil-org-mode-map <normal-state> g r
 ;; org-ctrl-c-ctrl-c
   ;; :general
