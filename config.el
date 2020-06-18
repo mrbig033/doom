e9d5ca8862bb2243133855a4a44629623a9bb23b
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-18 08:36:35 -0300

    config.el

--- config.el
+++ config.el
@@ -1,49 +1,39 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq user-full-name "mrbig"
-      user-mail-address "mrbig033@protonmail.com")
+(setq! eldoc-idle-delay 2
+       my-lisp "~/.doom.d/ml"
+       org-directory "~/org/"
+       user-full-name "mrbig"
+       confirm-kill-emacs nil
+       auto-revert-verbose nil
+       my-kbd "~/.doom.d/ml/kbd"
+       trash-directory "~/.Trash"
+       use-package-always-defer t
+       flyspell-correct-auto-delay 1
+       display-line-numbers-type nil
+       recentf-auto-cleanup "11:59pm"
+       +word-wrap-extra-indent 'single
+       custom-file "~/.doom.d/.custom-file.el"
+       abbrev-file-name "~/.doom.d/etc/abbrev.el"
+       user-mail-address "mrbig033@protonmail.com"
+       flycheck-global-modes '(not emacs-lisp-mode)
+       langtool-language-tool-jar "~/maps/langtool/langtooljar"
+       doom-theme 'doom-solarized-light
+       doom-font (font-spec :family "Input Mono" :size 20)
+       doom-big-font (font-spec :family "Input Mono" :size 20)
+       doom-unicode-font (font-spec :family "Input Mono" :size 20)
+       doom-variable-pitch-font (font-spec :family "Input Mono"))
 
-(setq doom-font (font-spec :family "Input Mono" :size 20)
-      doom-variable-pitch-font (font-spec :family "Input Mono") ; inherits `doom-font''s :size
-      doom-unicode-font (font-spec :family "Input Mono" :size 20)
-      doom-big-font (font-spec :family "Input Mono" :size 20))
-
-(setq doom-theme 'doom-solarized-light)
-
-(setq org-directory "~/org/")
-
-(setq display-line-numbers-type nil)
+(setq-default recentf-max-saved-items 20)
+(setq-hook! 'eww-mode-hook display-buffer-alist nil)
 
 (put 'narrow-to-region 'disabled nil)
 (put 'customize-group 'disabled nil)
 
-(setq-hook! 'eww-mode-hook display-buffer-alist nil)
 
-(setq-default recentf-max-saved-items 20)
-
-(setq! my-lisp "~/.doom.d/ml"
-       my-kbd "~/.doom.d/ml/kbd"
-       use-package-always-defer t
-       recentf-auto-cleanup "11:59pm"
-       +word-wrap-extra-indent 'single
-       confirm-kill-emacs nil
-       auto-revert-verbose nil
-       eldoc-idle-delay 2
-       trash-directory "~/.Trash"
-       flyspell-correct-auto-delay 1
-       custom-file "~/.doom.d/.custom-file.el"
-       abbrev-file-name "~/.doom.d/etc/abbrev.el"
-       flycheck-global-modes '(not emacs-lisp-mode)
-       langtool-language-tool-jar "~/maps/langtool/langtooljar")
+(mapc 'load (file-expand-wildcards "/Users/davi/.doom.d/ml/kbd/*.el"))
+(mapc 'load (file-expand-wildcards "/Users/davi/.doom.d/ml/*.el"))
 
 (global-flycheck-mode -1)
 (global-subword-mode +1)
 (mouse-avoidance-mode 'jump)
-
-(defun my-buffer-predicate (buffer)
-  (if (string-match "\*" (buffer-name buffer)) nil t))
-
-(set-frame-parameter nil 'buffer-predicate 'my-buffer-predicate)
-
-(mapc 'load (file-expand-wildcards "/Users/davi/.doom.d/ml/kbd/*.el"))
-(mapc 'load (file-expand-wildcards "/Users/davi/.doom.d/ml/*.el"))
