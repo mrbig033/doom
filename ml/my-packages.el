2ca2ccf83753c74705767d108d13833337d967ea
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-18 08:41:48 -0300

    ml/my-packages.el

--- ml/my-packages.el
+++ ml/my-packages.el
@@ -1258,6 +1258,7 @@
         :n "<escape>" 'my-quiet-save-buffer
         :map (lisp-interaction-mode-map)
         :n "<escape>" 'ignore))
+
 (use-package! git-auto-commit-mode
   :custom
   ;; (gac-debounce-interval (* 60 10))
