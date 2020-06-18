2a786fe87ead5bc4b8b4abd3074d0fe41626d22d
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-18 10:16:58 -0300

    ml/kbd/keys.el

--- ml/kbd/keys.el
+++ ml/kbd/keys.el
@@ -52,38 +52,40 @@
       :nvg "C-S-p"    'cool-moves-word-backwards
       )
 ;; GLOBALS
-(map! "M--"           'winner-undo
-      "M-             '"   'winner-undo
-      "M-="           'winner-redo
-      "M-+"           'winner-redo
-      "M-/"           'hippie-expand
-      "C-x p"         'check-parens
-      "C-;"           'helpful-at-point
-      "M-RET"         'my-indent-buffer
-      "C-9"           'evilnc-comment-or-uncomment-lines
-      "C-c v"         'vlf
-      "M-9"           'delete-other-windows
-      "M-0"           'quit-window
-      "M-n"           'my-forward-paragraph-do-indentation
-      "M-p"           'my-backward-paragraph-do-indentation
-      "M-]"           'evil-window-prev
-      "M-["           'evil-window-next
-      "C-c h"         'previous-buffer
-      "C-c y"         'kill-ring-save
-      "C-c l"         'next-buffer
-      "C-c m"         'define-mode-abbrev
-      "C-c M"         'define-global-abbrev
-      "C-             '"   'org-cycle-agenda-files
-      "C-c u"         'redraw-display
-      "C-c a"         'my-goto-agenda
-      "C-l"           'recenter-top-bottom
-      "C-c d"         'ispell-change-dictionary
-      "C-c o"         'my-org-force-open-other-window
-      "C-h e"         'describe-package
-      "C-h N"         'my-show-server-name
-      "C-c t"         'git-timemachine
-      "C-c -"         'insert-char
-      "C-c i"         'emacs-init-time)
+(map!
+
+ "C-'"   'org-cycle-agenda-files
+ "C-9"           'evilnc-comment-or-uncomment-lines
+ "C-;"           'helpful-at-point
+ "C-c -"         'insert-char
+ "C-c M"         'define-global-abbrev
+ "C-c a"         'my-goto-agenda
+ "C-c d"         'ispell-change-dictionary
+ "C-c h"         'previous-buffer
+ "C-c i"         'emacs-init-time
+ "C-c l"         'next-buffer
+ "C-c m"         'define-mode-abbrev
+ "C-c o"         'my-org-force-open-other-window
+ "C-c t"         'git-timemachine
+ "C-c u"         'redraw-display
+ "C-c v"         'vlf
+ "C-c y"         'kill-ring-save
+ "C-h N"         'my-show-server-name
+ "C-h e"         'describe-package
+ "C-l"           'recenter-top-bottom
+ "C-x p"         'check-parens
+ "M-+"           'winner-redo
+ "M--"           'winner-undo
+ "M-/"           'hippie-expand
+ "M-0"           'quit-window
+ "M-9"           'delete-other-windows
+ "M-="           'winner-redo
+ "M-RET"         'my-indent-buffer
+ "M-["           'evil-window-next
+ "M-]"           'evil-window-prev
+ "M-n"           'my-forward-paragraph-do-indentation
+ "M-p"           'my-backward-paragraph-do-indentation
+ )
 ;; LEADER KEY
 (map! :leader "sw"    'wordnut-lookup-current-word
       :leader "sW"    'wordnut-search
