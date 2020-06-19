c581759177b031d4bea7a0e4fd854c185e18ca47
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-18 21:13:51 -0300

    ml/kbd/local_kbd.el

--- ml/kbd/local_kbd.el
+++ ml/kbd/local_kbd.el
@@ -5,12 +5,6 @@
       (:map helpful-mode-map
        :nvig "C-r" 'helpful-update)
 
-      (:map (evil-org-mode-map org-mode-map global-map)
-       :nvig "M-k"   'windmove-up
-       :nvig "M-j"   'windmove-down
-       :nvig "M-h"   'windmove-left
-       :nvig "M-l"   'windmove-right)
-
       (:map (minibuffer-local-map
              minibuffer-local-ns-map
              minibuffer-local-completion-map
