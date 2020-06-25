c904a4ad5a7b101e7776f723ceb6eee9f9092ebb
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-25 08:40:51 -0300

    ml/kbd/global_kbd.el

--- ml/kbd/global_kbd.el
+++ ml/kbd/global_kbd.el
@@ -1,32 +1,33 @@
 ;; GLOBALS
-(map! "M-/"   'hippie-expand
-      "C-x p" 'check-parens
-      "C-;"   'helpful-at-point
-      "M-RET" 'my-indent-buffer
-      "C-c B" 'my-brain-commands
+(map! "M-/"    'hippie-expand
+      "C-x p"  'check-parens
+      "C-;"    'helpful-at-point
+      "M-RET"  'my-indent-buffer
+      "C-c B"  'my-brain-commands
       "<f8>"   'my-goto-brain
-      "C-c v" 'vlf
-      "M-9"   'delete-other-windows
-      "M-0"   'quit-window
-      "M-n"   'my-forward-paragraph-do-indentation
-      "M-p"   'my-backward-paragraph-do-indentation
-      "C-c h" 'previous-buffer
-      "C-c y" 'kill-ring-save
-      "C-c l" 'next-buffer
-      "C-c m" 'define-mode-abbrev
-      "C-c M" 'define-global-abbrev
-      "C-'"   'org-cycle-agenda-files
-      "C-c u" 'redraw-display
-      "C-l"   'recenter-top-bottom
-      "M--"   'winner-undo
-      "M-="   'winner-redo
-      "C-c q" 'quick-calc
-      "C-c d" 'ispell-change-dictionary
-      "C-c o" 'my-org-force-open-other-window
-      "C-h e" 'describe-package
-      "C-h N" 'my-show-server-name
-      "C-c t" 'git-timemachine
-      "C-c i" 'emacs-init-time)
+      "<M-f8>" 'my-goto-brain-same-window
+      "C-c v"  'vlf
+      "M-9"    'delete-other-windows
+      "M-0"    'quit-window
+      "M-n"    'my-forward-paragraph-do-indentation
+      "M-p"    'my-backward-paragraph-do-indentation
+      "C-c h"  'previous-buffer
+      "C-c y"  'kill-ring-save
+      "C-c l"  'next-buffer
+      "C-c m"  'define-mode-abbrev
+      "C-c M"  'define-global-abbrev
+      "C-'"    'org-cycle-agenda-files
+      "C-c u"  'redraw-display
+      "C-l"    'recenter-top-bottom
+      "M--"    'winner-undo
+      "M-="    'winner-redo
+      "C-c q"  'quick-calc
+      "C-c d"  'ispell-change-dictionary
+      "C-c o"  'my-org-force-open-other-window
+      "C-h e"  'describe-package
+      "C-h N"  'my-show-server-name
+      "C-c t"  'git-timemachine
+      "C-c i"  'emacs-init-time)
 
 (general-define-key
  :keymaps 'override
