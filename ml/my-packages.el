ee2d6a30d21539445e55ab37c1bdbebc5c35c8e2
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-25 05:46:58 -0300

    ml/my-packages.el

--- ml/my-packages.el
+++ ml/my-packages.el
@@ -1459,29 +1459,29 @@
     (doom/window-maximize-vertically))
 
 
-  (defun org-brain--vis-text (entry)
-    "Insert text of ENTRY. Helper function for `org-brain-visualize'."
-    (if-let ((text (org-brain-text entry)))
-        (progn
-          (setq text (string-trim text))
-          (if (or (boundp 'org-brain-polymode)
-                  org-brain-show-full-entry
-                  (> (length text) 0))
-              (progn
-                (insert "\n\n")
-                (setq org-brain--vis-entry-text-marker (point-marker))
-                (insert "--- \n\n")
-                (run-hooks 'org-brain-after-visualize-hook)
-                (insert (with-temp-buffer
-                          (insert text)
-                          (delay-mode-hooks
-                            (org-mode)
-                            (setq-local org-pretty-entities t)
-                            (font-lock-ensure (point-min) (point-max))
-                            (buffer-string))))
-                (run-hooks 'org-brain-visualize-text-hook))
-            (run-hooks 'org-brain-after-visualize-hook)))
-      (run-hooks 'org-brain-after-visualize-hook)))
+  ;; (defun org-brain--vis-text (entry)
+  ;;   "Insert text of ENTRY. Helper function for `org-brain-visualize'."
+  ;;   (if-let ((text (org-brain-text entry)))
+  ;;       (progn
+  ;;         (setq text (string-trim text))
+  ;;         (if (or (boundp 'org-brain-polymode)
+  ;;                 org-brain-show-full-entry
+  ;;                 (> (length text) 0))
+  ;;             (progn
+  ;;               (insert "\n\n")
+  ;;               (setq org-brain--vis-entry-text-marker (point-marker))
+  ;;               (insert "--- \n\n")
+  ;;               (run-hooks 'org-brain-after-visualize-hook)
+  ;;               (insert (with-temp-buffer
+  ;;                         (insert text)
+  ;;                         (delay-mode-hooks
+  ;;                           (org-mode)
+  ;;                           (setq-local org-pretty-entities t)
+  ;;                           (font-lock-ensure (point-min) (point-max))
+  ;;                           (buffer-string))))
+  ;;               (run-hooks 'org-brain-visualize-text-hook))
+  ;;           (run-hooks 'org-brain-after-visualize-hook)))
+  ;;     (run-hooks 'org-brain-after-visualize-hook)))
 
 
   (add-hook 'before-save-hook #'org-brain-ensure-ids-in-buffer))
