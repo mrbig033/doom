0e33fdc59999043d31545efbcb718aa5a5626577
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-25 03:45:57 -0300

    init.el

--- init.el
+++ init.el
@@ -144,7 +144,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pomodoro)               ; organize your plain life in plain text
+       (org +pomodoro
+            +brain)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
