00a7c7fbcf1527fb9e4fd205bc875fa9fa6cef42
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-26 05:48:40 -0300

    ml/functions.el

--- ml/functions.el
+++ ml/functions.el
@@ -356,15 +356,3 @@ is already narrowed."
 (defun my-goto-brain-game ()
   (interactive)
   (org-brain-visualize "game"))
-
-(defun my-poetry-flush ()
-  (interactive)
-  (poetry--setup-windows)
-  (remove-overlays (point-min) (point-max) 'poetry t)
-  (run-with-timer 0 nil
-                  (lambda (buf)
-                    (with-current-buffer buf
-                      (with-silent-modifications
-                        (remove-text-properties
-                         (point-min) (point-max) '(fontified)))))
-                  (current-buffer)))
