051b3f97005c751bea97ba3cdfffece3ef7ce32b
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-26 00:13:31 -0300

    ml/functions.el

--- ml/functions.el
+++ ml/functions.el
@@ -328,3 +328,25 @@
   (interactive)
   (switch-to-buffer "*Messages*"))
 
+;; https://endlessparentheses.com/emacs-narrow-or-widen-dwim.html
+(defun my-narrow-or-widen-dwim (p)
+  "Widen if buffer is narrowed, narrow-dwim otherwise.
+With prefix P, don't widen, just narrow even if buffer
+is already narrowed."
+  (interactive "P")
+  (declare (interactive-only))
+  (cond ((and (buffer-narrowed-p) (not p)) (widen))
+        ((region-active-p)
+         (narrow-to-region (region-beginning)
+                           (region-end)))
+        ((derived-mode-p 'org-mode)
+         ;; `org-edit-src-code' is not a real narrowing
+         ;; command. Remove this first conditional if
+         ;; you don't want it.
+         (cond ((ignore-errors (org-edit-src-code) t)
+                (delete-other-windows))
+               ((ignore-errors (org-narrow-to-block) t))
+               (t (org-narrow-to-subtree))))
+        ((derived-mode-p 'latex-mode)
+         (LaTeX-narrow-to-environment))
+        (t (narrow-to-defun))))
