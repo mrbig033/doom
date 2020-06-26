89c67a0cd4e3bc88bda604ac756da157a1ec12d9
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-26 06:26:20 -0300

    ml/kbd/leader_kbd.el

--- ml/kbd/leader_kbd.el
+++ ml/kbd/leader_kbd.el
@@ -52,6 +52,7 @@
       :leader "nn"     'my-narrow-or-widen-dwim
       :leader "nw"     'my-widen-to-center
       :desc "Counsel Ag" :leader "sg"     'counsel-ag
+      :desc "Dic. Informal " :leader "si"     'engine/search-dic-informal
       :leader "ww"     'doom/window-maximize-vertically
       :leader "x"      'eyebrowse-close-window-config
       :leader "z"      'hydra-window/body
