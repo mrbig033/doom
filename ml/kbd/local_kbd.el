4b63619d321bb151b6cd1ab4a7b5080c7d3f49bc
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-06-26 02:28:49 -0300

    ml/kbd/local_kbd.el

--- ml/kbd/local_kbd.el
+++ ml/kbd/local_kbd.el
@@ -38,27 +38,29 @@
        :e "C-r"      'deft-refresh-filter)
 
       (:map (org-brain-visualize-mode-map)
-       :e "<C-return>"    'my-brain-push-and-open
-       :e "SPC d"    'my-search-ag-brain
-       :e "D"        'org-brain-add-entry
-       :e "m"        'push-button
-       :e "C-."      'counsel-brain
-       :e "G"        'end-of-buffer
-       :e "C-x b"    'my-brain-commands
-       :e "C-l"      'link-hint-open-link
-       :e "gg"       'beginning-of-buffer
-       :e "ç"        'org-brain-prefix-map
-       :e "O"        'org-brain-goto-current
-       :e "r"        'org-brain-rename-file
-       :e "R"        'org-brain-open-resource
-       :e "<f9>"     'org-brain-visualize-quit
-       :e "<f8>"   'org-brain-visualize-quit
-       :e "<escape>" 'org-brain-visualize-quit
-       :e ","        'avy-goto-char-2
-       :e "."        'org-brain-visualize-mind-map
-       :e "i"        'org-brain-switch-brain
-       :e "C-r"      'org-brain-update-id-locations
-       :e "o"        'my-brain-goto-current-maximize)
+       :e "<C-return>"  'my-brain-push-and-open
+       :e "SPC d"       'my-search-ag-brain
+       :e "D"           'org-brain-add-entry
+       :e "m"           'push-button
+       :e "C-."         'counsel-brain
+       :e "G"           'end-of-buffer
+       :e "C-x b"       'my-brain-commands
+       :e "C-l"         'link-hint-open-link
+       :e "gg"          'beginning-of-buffer
+       :e "ç"           'org-brain-prefix-map
+       :e "O"           'org-brain-goto-current
+       :e "r"           'org-brain-rename-file
+       :e "R"           'org-brain-open-resource
+       :e "<f9>"        'org-brain-visualize-quit
+       :e "<f8>"        'org-brain-visualize-quit
+       :e "<escape>"    'ignore
+       :e "q"           'ignore
+       :e ","           'avy-goto-char-2
+       :e "."           'org-brain-visualize-mind-map
+       :e "<backspace>" 'org-brain-visualize-mind-map
+       :e "i"           'org-brain-switch-brain
+       :e "C-r"         'org-brain-update-id-locations
+       :e "o"           'my-brain-goto-current-maximize)
 
       (:map (messages-buffer-mode-map)
        :ng "<escape>" 'ignore
