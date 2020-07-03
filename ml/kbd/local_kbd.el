c10d58a2e6f269f8e70671207aca2f9a57f13dd0
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-03 09:30:33 -0300

    ml/kbd/local_kbd.el

--- ml/kbd/local_kbd.el
+++ ml/kbd/local_kbd.el
@@ -86,4 +86,18 @@
             :nveg "q"      'quit-window)
 
       (:map (fountain-mode-map)
-            :nvi "TAB" 'fountain-dwi k
+            :nvi "TAB" 'fountain-dwim
+            :nv "C-;"  'fountain-upcase-line-and-newline
+            :nv "c-n"  'fountain-forward-character
+            :nv "c-p"  'fountain-backward-character
+            :nv "gh"   'fountain-forward-scene
+            :nv "gj"   'fountain-outline-next
+            :nv "gk"   'fountain-outline-previous
+            :nv "gl"   'fountain-backward-scene
+            :nv "m-n"  'fountain-forward-scene
+            :nv "m-p"  'fountain-backward-scene
+            :nv "zi"   'fountain-outline-show-all
+            :nv "zm"   'fountain-outline-cycle-global)
+
+      (:map (custom-mode-map compilation-mode-map)
+            :n "<escape>" 'evil-force-normal-state))
