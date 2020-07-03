cf4f54b7dde64b850b24843d4c75eb055bf2403a
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-03 14:56:04 -0300

    ml/kbd/misc_kbd.el

--- ml/kbd/misc_kbd.el
+++ ml/kbd/misc_kbd.el
@@ -5,47 +5,46 @@
 ;;       :map org-agenda-mode-map)
 
 (map! (:map (pabbrev-mode-map pabbrev-select-mode-map)
-            :i "C-l" 'pabbrev-expand-maybe)
+       :i "C-l" 'pabbrev-expand-maybe)
 
       (:map helpful-mode-map
-            :nvig "C-r" 'helpful-update
-            :n "<escape>" 'quit-window)
+       :nvig "C-r" 'helpful-update
+       :n "<escape>" 'quit-window)
 
       (:map (org-mode-map evil-org-mode-map)
-            :nvig "C-c C-s" 'org-emphasize
-            :v "<insert>" 'org-insert-link)
+       :nvig "C-c C-s" 'org-emphasize
+       :v "<insert>" 'org-insert-link)
 
       (:map (markdown-mode-map)
-            "C-c ," 'my-engine-rhymit-pt
-            "C-c ." 'my-engine-dic-informal-rimas)
+       "C-c ," 'my-engine-rhymit-pt
+       "C-c ." 'my-engine-dic-informal-rimas)
 
       (:map (org-brain-prefix-map)
-            "D" 'org-brain-add-entry
-            "E" 'my-brain-erase-history)
+       "D" 'org-brain-add-entry
+       "E" 'my-brain-erase-history)
 
       (:map (flyspell-mode-map)
-            :n "-" 'endless/ispell-word-then-abbrev
-            :n "z-" 'flyspell-correct-word)
+       :n "-" 'endless/ispell-word-then-abbrev
+       :n "z-" 'flyspell-correct-word)
 
       (:map (messages-buffer-mode-map)
-            :ng "<escape>" 'ignore
-            :nveg "q"      'quit-window)
+       :ng "<escape>" 'ignore
+       :nveg "q"      'quit-window)
 
       (:map (emacs-lisp-mode-map
              prog-mode-map
              text-mode-map
              conf-mode-map)
-            :n "<escape>" 'my-quiet-save-buffer)
+       :n "<escape>" 'my-quiet-save-buffer)
 
       (:map (snippet-mode-map)
-            :n "<escape>" 'ignore
-            :n "q" 'quit-window)
+       :n "<escape>" 'ignore
+       :n "q" 'quit-window)
 
       (:after projectile
        :map (projectile-mode-map)
-            :nv "H"   'projectile-previous-project-buffer
-            :nv "L"   'projectile-next-project-buffer)
+       :nv "H"   'projectile-previous-project-buffer
+       :nv "L"   'projectile-next-project-buffer)
 
       (:map (custom-mode-map compilation-mode-map)
-            :n "<escape>" 'evil-normal-state))
-
+       :n "<escape>" 'evil-normal-state))
