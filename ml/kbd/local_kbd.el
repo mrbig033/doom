abf35989ac0e542a69520a0f5884084cf7f6568d
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-03 10:06:16 -0300

    ml/kbd/local_kbd.el

--- ml/kbd/local_kbd.el
+++ ml/kbd/local_kbd.el
@@ -85,6 +85,29 @@
             :ng "<escape>" 'ignore
             :nveg "q"      'quit-window)
 
+      (:map (python-mode-map)
+            "M-p"              'my-backward-paragraph-do-indentation
+            "M-n"              'my-forward-paragraph-do-indentation
+            "C-c ç"            'my-python-shebang
+            "C-ç"              'elpy-shell-switch-to-shell
+            "M-a"              'python-nav-backward-statement
+            "M-e"              'python-nav-forward-statement
+            :n "<return>"      'hydra-python-mode/body
+            :i "C-="           'my-python-colon-newline
+            :nv "<"            'python-indent-shift-left
+            :nv ">"            'python-indent-shift-right
+            :nvig "<C-return>" 'my-quickrun)
+
+      (:map (inferior-python-mode-map)
+            "C-ç" 'my-elpy-switch-to-buffer
+            :i "C-l" 'comint-clear-buffer)
+
+      (:map (prog-mode-map text-mode-map)
+            :n "<escape>" 'my-quiet-save-buffer)
+
+      (:map (snippet-mode-map)
+            :n "<escape>" 'ignore)
+
       (:map (fountain-mode-map)
             :nvi "TAB" 'fountain-dwim
             :nv "C-;"  'fountain-upcase-line-and-newline
