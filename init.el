7a60bd53f5bdaf6534101e3324540454f070c676
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-04 21:46:48 -0300

    init.el

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
        ;;japanese
 
        :completion
-       (company +childframe)          ; the ultimate code completion backend
+       (company)          ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy +prescient)               ; a search engine for love and life
