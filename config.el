f819a9720237d2a04b5f19df401245b368cdb08f
mrbig033 <mrbig033@protonmail.com@protonmail.com>
2020-07-24 23:39:39 -0300

    automatic commit

--- config.el
+++ config.el
@@ -59,51 +59,116 @@
 ;; they are implemented.
 
 ;; (use-package! evil
-;; :config
+;;   :config
+;;   (map! )
 
 (defun my-last-buffer ()
 (interactive)
 (switch-to-buffer nil))
 (map! :desc "Last Buffer" :nvg "M-s" 'my-last-buffer)
 
-(map! :n "'"       'evil-goto-mark
-      :n "gm"      'evil-set-marker
-      :leader "fO" 'counsel-locate
+(use-package! ivy
+  :custom
+  (ivy-height 15)
+  (counsel-grep-swiper-limit 300000)
+  (ivy-extra-directories nil)
+  (counsel-outline-display-style 'title)
+  (counsel-find-file-at-point t)
+  (counsel-bookmark-avoid-dired t)
+  (ivy-count-format "")
+
+  (ivy-ignore-buffers '("^#.*#$"
+                        "^\\*.*\\*"
+                        "^agenda.org$"
+                        "magit"
+                        "*org-src-fontification.\\*"))
+
+  (counsel-ag nil "~/.emacs.d/" "-G '.org'")
+
+  ;; Original:
+  ;; (setq counsel-ag-base-command "ag --vimgrep %s")
+
+
+  ;; (counsel-ag-base-command "ag
+  ;; --filename
+  ;; --nocolor
+  ;; --nogroup
+  ;; --smart-case
+  ;; --skip-vcs-ignores
+  ;; --silent
+  ;; --ignore
+  ;; '*.elc'
+  ;; %s")
+
+  :config
+
+  (map! :nvi "C-s" 'counsel-grep-or-swiper
+        :nvi "M-y" 'counsel-yank-pop
+        :nvi "C-," 'counsel-projectile-switch-to-buffer
+        :nvi "C-<" 'ivy-switch-buffer
+        :nvi "C-." 'counsel-M-x
+        :nvi "M-u" 'yas-insert-snippet
+        :nvi "C-s" 'counsel-grep-or-swiper
+        :nvi "C-." 'ivy-next-line
+        :nvi "C-," 'counsel-find-symbol
+        :desc "Counsel Ag" :leader "sg"  'counsel-ag)
+
+
+  (map! :map (ivy-minibuffer-map ivy-switch-buffer-map)
+        "M-y"      'ivy-next-line
+        "M-r"      'ivy-next-line
+        "C-,"      'ivy-next-line
+        "C-."      'ivy-next-line
+        "C-/"      'ivy-next-line
+        "C-j"      'ivy-immediate-done
+        "M-q"      'ivy-done
+        "C-k"      'kill-line
+        "C-d"      'delete-char
+        "C-h"      'delete-backward-char
+        "C-w"      'backward-kill-word
+        "<insert>" 'yank))
+
+;; https://github.com/abo-abo/swiper/issues/2588#issuecomment-637042732
+(setq swiper-use-visual-line-p #'ignore)
+
+(defun counsel-ag-thing-at-point ()
+  (interactive)
+  (ivy-with-thing-at-point 'counsel-ag))
+
+(map! :leader "fO" 'counsel-locate
       :leader "fp" 'search-packages
       :leader "fs" 'search-settings)
 
 (use-package! helpful
+  :demand t
   :config
   (map! (:map helpful-mode-map
          :nvig "C-r" 'helpful-update
          :nvig "C-;" 'helpful-at-point
          :n "<escape>" 'quit-window)))
 
-(use-package! prog-mode
-              :config
+(use-package! prog-mode 
+  :config
+
+(defun my-indent-buffer ()
+    (interactive)
+    (let ((inhibit-message t))
+      (evil-indent
+       (point-min)
+       (point-max))))
 
   (map!  "M-RET"          'my-indent-buffer
          :n "<backspace>" 'org-edit-src-exit
          (:map (prog-mode-map
                    emacs-lisp-mode-map
                    lisp-mode-map))
-  (defun my-evil-sel-to-end ()
-    (interactive)
-    (evil-visual-char)
-    (evil-last-non-blank))
+  ; (defun my-evil-sel-to-end ()
+  ;   (interactive)
+  ;   (evil-visual-char)
+  ;   (evil-last-non-blank))
 
 
-  (defun my-recenter-window ()
-    (interactive)
-    (recenter-top-bottom
-     `(4)))
-
-  (defun my-indent-buffer ()
-    (interactive)
-    (let ((inhibit-message t))
-      (evil-indent
-       (point-min)
-       (point-max))))))
+  ))
 
 (use-package! files
   :custom
@@ -115,7 +180,14 @@
     (let ((inhibit-message t))
       (evil-ex-nohighlight)
       (save-buffer)))
-  (map! :n "<escape>" 'my-quiet-save-buffer)
+
+(defun my-sel-to-end ()
+  (interactive)
+  (evil-visual-char)
+  (evil-last-non-blank))
+
+  (map! (:map prog-mode-map tex-main-file)
+        :n "<escape>" 'quiet-save-buffer)
 
   (defun goto-python-scratch ()
     (interactive)
@@ -125,6 +197,10 @@
     (interactive)
     (counsel-ag nil "~/.doom.d/.searches/" "-f -G '.org'"))
 
+  (defun my-recenter-window ()
+    (interactive)
+    (recenter-top-bottom
+     `(4)))
 
   (defun goto-packages ()
     (interactive)
@@ -165,9 +241,15 @@
                                         (list l1 l2)))))
         (reverse-region beg end)))))
 
-(map! :nvieg "M-9" 'quit-window
-      :leader "0" 'delete-window
-      :leader "k" 'kill-this-buffer)
+(general-define-key
+:states  '(global normal visual insert)
+:keymaps 'override
+"M-9" 'quit-window
+"M-s" 'my-last-buffer)
+
+(map! :i      "M-e" 'yas-expand
+      :leader "0"   'delete-window
+      :leader "k"   'kill-this-buffer)
 
 (use-package! org
   :after-call after-find-file
@@ -208,6 +290,21 @@
          :nvi  "<C-return>" 'my-open-two-lines
          :nvi  "M-n"        'my-forward-paragraph-do-indentation
          :nvi  "M-p"        'my-backward-paragraph-do-indentation)))
+
+(defun xah-clean-empty-lines ()
+  "replace repeated blank lines to just 1."
+  (interactive)
+  (let ($begin $end)
+    (if (region-active-p)
+        (setq $begin (region-beginning) $end (region-end))
+      (setq $begin (point-min) $end (point-max)))
+    (save-excursion
+      (save-restriction
+        (narrow-to-region $begin $end)
+        (progn
+          (goto-char (point-min))
+          (while (re-search-forward "\n\n\n+" nil "move")
+            (replace-match "\n\n")))))))
 
 (use-package! company
   :custom
